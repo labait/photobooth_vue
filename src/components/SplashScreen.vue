56ff44b139fec496258f423009f6ed132c84db2a
Federica Milesi <fede.arctic@gmail.com>
2026-04-22 11:51:37 +0200

    fix: adjust layout and styling for SplashScreen component

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -2,23 +2,19 @@
 import polaroid from './Polaroid.vue';
 import Header from './Header.vue';
 import { ref, inject } from 'vue';
-
 const global = inject('global');
 </script>
 
 <template>
-  <div class="flex flex-col justify-start w-full h-full">
-    <div class="grid grid-cols-2 gap-4 items-center">
-      <p class="text-white font-bold text-[8vw] md:text-[6vw] py-2">
+  <div class="flex flex-col justify-center w-full min-h-screen px-6 md:px-12 lg:px-20">
+    <div class="grid grid-cols-2 gap-4 items-center w-full">
+      <p class="text-white font-bold text-[8vw] md:text-[6vw] lg:text-[5vw] py-2">
         LABA'S<br>PHOTOBOOTH
       </p>
       <div class="flex justify-center mb-6 md:mb-8">
-        <img src="../assets/croniche-disorganiche.png" class="h-auto object-contain max-w-sm">
-        <!-- commento  <img src="../assets/txt.svg" class="w-full h-auto object-cover block">
-        <img src="../assets/polaroid.svg" class="w-full h-auto object-cover block"> -->
+        <img src="../assets/croniche-disorganiche.png" class="h-auto object-contain max-w-sm w-full">
       </div>
     </div>
-
     <div class="flex relative z-20 items-center justify-start mb-16">
       <router-link
         v-if="global.features.camera"
@@ -26,7 +22,6 @@ const global = inject('global');
         class="btn-primary mr-4">
         Scopri l'esperienza
       </router-link>
-
       <router-link
         v-if="global.features.camera"
         to="/list"
